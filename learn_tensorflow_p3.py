39c16dc231cb99cc67defe849ae3b74fe42ad0f4
ThinhPTran <trphthinh@gmail.com>
2016-12-07 19:37:22 +0700

    first commit with IntelliJ

--- learn_tensorflow_p3.py
+++ learn_tensorflow_p3.py
@@ -15,7 +15,17 @@ feed forward + backprop = epoch
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+'''
+The MNIST data is split into three parts: 55,000 data points
+ of training data (mnist.train), 10,000 points of test data 
+ (mnist.test), and 5,000 points of validation data 
+ (mnist.validation). This split is very important: 
+ it's essential in machine learning that we have separate data 
+ which we don't learn from so that we can make sure that 
+ what we've learned actually generalizes!
+'''
+
+mnist = input_data.read_data_sets("/home/thinhptran/Programming/LearnDeepLearning", one_hot=True)
 
 # 10 classes, 0-9
 '''
@@ -30,7 +40,7 @@ n_nodes_hl3 = 500
 n_classes = 10
 batch_size = 100
 
-# height x width
+# height x width: 784 = 28x28
 x = tf.placeholder('float', [None, 784])
 y = tf.placeholder('float')
 
@@ -39,8 +49,8 @@ y = tf.placeholder('float')
 def neural_network_model(data):
 
 	# (inputdata * weights) + biases  
-	hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-	               'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+	hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1]))
+	               ,'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
 	hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2]))
 	               ,'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
@@ -73,20 +83,34 @@ def neural_network_model(data):
 #specify how data runs through the model
 def train_neural_network(x):
 	prediction = neural_network_model(x)
+
+	'''
+	cost function is used in below form for the numerical stability purposes. 
+	'''
 	cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y))
 
-	#learning rate = 0.001
+	#learning rate = 0.001 
+	# Optimizer can be call train_step 
 	optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 	# cycles  of feedforward + backprop
 	hm_epochs = 10
 
 	with tf.Session() as sess:
+		'''
+		Now we have our model set up to train. One last thing
+		before we launch it, we have to create an operation to 
+		initialize the variables we created. Note that this defines
+		the operation but does not run it yet:
+		'''	
 		sess.run(tf.initialize_all_variables())
 
 		for epoch in range(hm_epochs):
+			# To measure loss, failure for each epoch
 			epoch_loss = 0
+			# Loop through training dataset 
 			for _ in range(int(mnist.train.num_examples/batch_size)):
+				# It is training dataset in a size of batch size
 				epoch_x, epoch_y = mnist.train.next_batch(batch_size)
 				_, c = sess.run([optimizer, cost], feed_dict = {x: epoch_x, y: epoch_y})
 				epoch_loss += c
