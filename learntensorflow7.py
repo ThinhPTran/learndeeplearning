424683b49fb9c321fc18d11c687caac41f937e9f
ThinhPTran <trphthinh@gmail.com>
2017-01-13 23:16:31 +0700

    understading assign

--- learntensorflow7.py
+++ learntensorflow7.py
@@ -10,10 +10,11 @@ run in a Session to take effect:
 
 # Create variable with starting value of 1
 my_var = tf.Variable(1)
+my_const = tf.constant(2, name="my_const")
 
 # Create an operation that multiplies the 
 # variable by 2 each time it is run 
-my_var_times_two = my_var.assign(my_var * 2)
+my_var_times_two = my_var.assign(tf.mul(my_var,my_const))
 
 # Initialization operation
 init = tf.global_variables_initializer()
@@ -30,6 +31,15 @@ with tf.Session() as sess:
 	# Out: 4
 
 	print(sess.run(my_var_times_two))
+	# Out: 8
+
+	writer = tf.summary.FileWriter("./my_graph4", sess.graph)
+	writer.close()
+
+	sess.close()
+
+
+
 
 
 
